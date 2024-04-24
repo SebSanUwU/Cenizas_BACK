03eb563d7fdd79ec810f7d520405e281d52642b1
Sebastian Camargo <92890055+SebSanUwU@users.noreply.github.com>
2024-04-24 14:53:02 -0500

    Update CenizasDelPasadoApplication.java

--- src/main/java/arsw/CenizasDelPasado/demo/CenizasDelPasadoApplication.java
+++ src/main/java/arsw/CenizasDelPasado/demo/CenizasDelPasadoApplication.java
@@ -35,7 +35,7 @@ public class CenizasDelPasadoApplication implements CommandLineRunner {
 			@Override
 			public void addCorsMappings(CorsRegistry registry) {
 				registry.addMapping("/**")
-						.allowedOrigins("http://localhost:4200")
+						.allowedOrigins("https://cenizasdelpasadogame.azurewebsites.net")
 						.allowedMethods("GET", "POST","PUT", "DELETE");
 			}
 		};
